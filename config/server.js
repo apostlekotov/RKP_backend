f24586af8a7a150ae092d276ce29875601921b3a
Kotov <kotov.smtp@gmail.com>
2020-10-29 15:06:04 +0200

    🛠 first init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7dbf683628022d2d791ff195c77ebd4e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '41e8ae47-bb26-4363-a3d8-266404636398'
+};
